6b352915224bc8258fb3ae4f6e50c9006e250ad0
Juan-Rodelo <juancamilo849@gmail.com>
2021-04-05 17:35:34 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,53 @@
+
+//Taking Module
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
+
+//Using createserver function from module
+http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello Node!\n');
+}).listen(8080);
+
+console.log('My first Node test server is running on Port 8080.');
+
+
+http.createServer((request, response) => {
+  let addr = request.url,
+    q = url.parse(addr, true),
+    filePath = 'http://localhost:8080';
+
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Added to log.');
+  }
+});
+
+if (q.pathname.includes('documentation')) {
+  filePath = (__dirname + '/documentation.html');
+} else {
+  filePath = 'index.html';
+}
+
+    if (q.pathname.includes('documentation')) {
+      filePath = (__dirname + '/documentation.html');
+    } else {
+      filePath = 'index.html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(data);
+      response.end();
+
+    });
+
+  }).listen(8080);
+  console.log('My test server is running on Port 8080.');
