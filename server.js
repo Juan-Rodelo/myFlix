b92bd4dcbe8c18f39ab7617848ce064e68eed699
Juan-Rodelo <juancamilo849@gmail.com>
2021-05-06 11:15:44 -0500

    Fixed Server (again)

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/movies/directors/:Name', passport.authenticate('jwt', {session : false
 
 //New users registration
 app.post('/users', [
-  check('Username', 'Username is required').isLenght({min: 5}),
+  check('Username', 'Username is required').isLength({min: 5}),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
   check('Email','Email does not appear to be valid').isEmail()
