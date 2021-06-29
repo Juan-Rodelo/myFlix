d6a95bbc870c6d6af90879cc843939777ae95671
Juan-Rodelo <juancamilo849@gmail.com>
2021-06-29 15:25:38 -0500

    Cors

--- server.js
+++ server.js
@@ -21,7 +21,18 @@ const Users = Models.User;
 
 const app = express();
 
-app.use(cors());
+let allowedOrigins = ['https://film-spelunker.netlify.app/', 'http://localhost:8080', 'http://localhost:1234'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //middleware function
 app.use(bodyParser.json());
